5f8c6e8beda5a9089427227863c4e1b4a9a4e14f
Wartemelon <artemmocrousov1@gmail.com>
2025-03-01 10:41:05 +0300

    fixes

--- user/add.c
+++ user/add.c
@@ -1,76 +1,51 @@
 #include "user/user.h"
 
+#define BUF_SIZE 256
+
+int 
+readNumber() {
+    char buf[BUF_SIZE];
+    int idx = 0;
+    char c;
+    int cc;
+
+    while ((cc = read(0, &c, 1)) > 0) {
+        if (c == ' ' || c == '\n') {
+            break;
+        }
+        if (idx >= BUF_SIZE - 1) {
+            fprintf(2, "Buffer size exceeded\n");
+            exit(1);
+        }
+        if (c < '0' || c > '9') {
+            fprintf(2, "Invalid number format\n");
+            exit(1);
+        }
+        buf[idx++] = c;
+    }
+
+    if (cc < 0) {
+        fprintf(2, "Error reading input\n");
+        exit(1);
+    }
+
+    // Если мы вышли из цикла сразу, ничего не записав в buf (idx == 0),
+    // значит пустой ввод или вообще не встретили цифр
+    if (idx == 0) {
+        fprintf(2, "Empty number\n");
+        exit(1);
+    }
+
+    buf[idx] = '\0';
+    return atoi(buf);
+}
+
 int 
 main(int argc, char *argv[]) 
 {
     int num1, num2;
-    char num1_str[256], num2_str[256];
-    int i = 0, j = 0;
-    int cc;
-    char c;
-    int is_num1_valid = 0, is_num2_valid = 0;
-
-    cc = read(0, &c, 1);
-    while (cc != 0) {
-        if (cc < 1) {
-            break;
-        }
-        if (i >= sizeof(num1_str) - 1) {
-            printf("%s", "Buffer size exceeded\n");
-            exit(1);
-        }
-        if (c == ' ' || c == '\n' || c == '\r') {
-            is_num1_valid = 1;
-            break;
-        }
-        if (c < '0' || c > '9') {
-            fprintf(2, "Invalid number format\n");
-            exit(1);
-        }
-        num1_str[i++] = c;
-        cc = read(0, &c, 1);
-    }
-    if (is_num1_valid == 0) {
-        fprintf(2, "Invalid number format\n");
-        exit(1);
-    }
-    num1_str[i] = '\0';
-    if (num1_str[0] == '\0') {
-        fprintf(2, "Empty number\n");
-        exit(1);
-    }
-    num1 = atoi(num1_str);
-
-    cc = read(0, &c, 1);
-    while (cc != 0) {
-        if (cc < 1) {
-            break;
-        }
-        if (j >= sizeof(num2_str) - 1) {
-            printf("%s", "Buffer size exceeded\n");
-            exit(1);
-        }
-        if (c == '\n' || c == '\r') {
-            is_num2_valid = 1;
-            break;
-        }
-        if (c < '0' || c > '9') {
-            fprintf(2, "Invalid number format\n");
-            exit(1);
-        }
-        num2_str[j++] = c;
-        cc = read(0, &c, 1);
-    }
-    if (is_num2_valid == 0) {
-        fprintf(2, "Invalid number format\n");
-        exit(1);
-    }
-    num2_str[j] = '\0';
-    if (num2_str[0] == '\0') {
-        fprintf(2, "Empty number\n");
-        exit(1);
-    }
-    num2 = atoi(num2_str);
+    num1 = readNumber();
+    num2 = readNumber();
 
     int sum = add(num1, num2); // Вызов системного вызова
     printf("%d\n", sum);
